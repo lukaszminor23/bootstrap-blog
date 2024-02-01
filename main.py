c4e530a97a7f27f3e60f2c1b50c7d7e4a452aed8
Łukasz Minor <lukaszminor23@gmail.com>
2024-02-01 15:47:41 +0100

    contact and about routes updated, footer and header included for consistency

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
