358bd3966c7a31d04841fc8ed56a60fd1e7afa7f
Łukasz Minor <lukaszminor23@gmail.com>
2024-02-15 16:23:37 +0100

    add now fetches post data from a database instead of an API

--- main.py
+++ main.py
@@ -1,21 +1,56 @@
 from flask import Flask, render_template, request
-from requests import get
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
 from smtplib import SMTP
 from email.message import EmailMessage
 
 
-POSTS_API = "https://api.npoint.io/d567145ab9835ad1db9e"
-response = get(POSTS_API).json()
 MY_EMAIL = "Your email"
 PASSWORD = "Your password"
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html", posts=response)
+    with app.app_context():
+        all_posts = db.session.execute(db.select(BlogPost)).scalars().all()
+    return render_template("index.html", posts=all_posts)
 
 
 @app.route("/contact", methods=['POST', 'GET'])
@@ -32,11 +67,10 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/post/<int:index>")
-def view_post(index):
-    for post in response:
-        if post["id"] == index:
-            return render_template("post.html", post=response[index - 1])
+@app.route("/post/<int:post_id>")
+def view_post(post_id):
+    requested_post = db.get_or_404(BlogPost, post_id)
+    return render_template("post.html", post=requested_post)
 
 
 def send_email(name, email, phone, message):
